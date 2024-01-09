945d773b41d861ba0a7a48d1b3c901bbca0a23b0
Suneetha-R <suneetharouthu000@gmai.com>
2024-01-09 12:14:44 +0000

    browser-history-solution

---
+++ App.js
@@ -0,0 +1,9 @@
+import './App.css'
+import History from './components/History'
+// These are the list used in the application. You can move them to any component needed.
+// eslint-disable-next-line no-unused-vars
+
+// Replace your code here
+const App = () => <History />
+
+export default App
